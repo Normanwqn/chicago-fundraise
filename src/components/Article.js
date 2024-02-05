cd94da6f3045380439b57fea51f3674c02e59054
normanqw <normanqw@umich.edu>
2024-02-04 21:37:51 -0500

    Iniital Testing

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Article() {
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Article Title</h1>
+            <p>This is an example of an article using Bootstrap for layout and design, making it fluid and responsive across devices.</p>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default Article;
